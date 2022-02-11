dfc653521a8bcb95e9d6e13d4d5ca697d7565445
Yuelin <yuelin.liu@mail.mcgill.ca>
2022-02-10 22:42:39 -0500

    normal

---
+++ A1/src/hal/Device.java
@@ -0,0 +1,18 @@
+/**
+ */
+package hal;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Device</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see hal.HalPackage#getDevice()
+ * @model
+ * @generated
+ */
+public interface Device extends EObject {
+} // Device
---
+++ A1/src/hal/impl/DeviceImpl.java
@@ -0,0 +1,39 @@
+/**
+ */
+package hal.impl;
+
+import hal.Device;
+import hal.HalPackage;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.eclipse.emf.ecore.impl.MinimalEObjectImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Device</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * @generated
+ */
+public class DeviceImpl extends MinimalEObjectImpl.Container implements Device {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected DeviceImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return HalPackage.Literals.DEVICE;
+	}
+
+} //DeviceImpl
