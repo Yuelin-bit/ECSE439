309724cd1fc2f2a7c7ff282998cc835ed27cd83b
Yuelin <yuelin.liu@mail.mcgill.ca>
2022-02-12 19:58:07 -0500

    add room

--- A1/src/ca/mcgill/emf/examples/hal/controller/HalController.java
+++ A1/src/ca/mcgill/emf/examples/hal/controller/HalController.java
@@ -1,8 +1,15 @@
 package ca.mcgill.emf.examples.hal.controller;
 
+import ca.mcgill.emf.examples.hal.*;
+import ca.mcgill.emf.examples.hal.application.HalApplication;
+
 public class HalController {
 
 	public static void addRoom(String name) {
-		
+		HalSystem halSystem = HalApplication.getHalSystem();
+		Room room = HalFactory.eINSTANCE.createRoom();
+		room.setName(name);
+		halSystem.getHome().get(0).getContains().get(0).setName(name);
+		HalApplication.save();
 	}
 }
